6ceccef83c0a8c4c349fea1df810c2589d70ef11
William Davidson <wj.davidson@outlook.com>
2024-10-16 09:18:18 +0200

    feat: added initial lucia auth config

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,7 @@
 import { NEON_DB_URL } from '$env/static/private'
 import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
+import * as schema from './schema'
 
 const client = neon(NEON_DB_URL)
-export const db = drizzle(client)
+export const db = drizzle(client, { schema })
