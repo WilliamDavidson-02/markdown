40491c1988639e56aa41b42bdf436653d17444da
William Davidson <wj.davidson@outlook.com>
2024-10-16 15:35:15 +0200

    feat: added email and password login

---
+++ src/routes/(auth)/login/+page.server.ts
@@ -0,0 +1,68 @@
+import type { Actions } from './$types'
+import { superValidate } from 'sveltekit-superforms'
+import { zod } from 'sveltekit-superforms/adapters'
+import { z } from 'zod'
+import { hash, verify } from '@node-rs/argon2'
+import { fail, redirect } from '@sveltejs/kit'
+import { lucia } from '$lib/server/auth'
+import { userTable } from '$lib/db/schema'
+import { eq } from 'drizzle-orm'
+import { db } from '$lib/db'
+
+const emailAndPasswordSchema = z.object({
+	email: z.string().email(),
+	password: z
+		.string()
+		.min(8, { message: 'Password must be at least 8 characters' })
+		.max(256, { message: 'Password must be at most 256 characters' })
+})
+
+export const load = async () => {
+	const form = await superValidate(zod(emailAndPasswordSchema))
+	return { form }
+}
+
+export const actions: Actions = {
+	default: async ({ request, cookies }) => {
+		const form = await superValidate(request, zod(emailAndPasswordSchema))
+
+		if (!form.valid) {
+			return fail(400, { form })
+		}
+
+		const email = form.data.email.trim().toLowerCase()
+		const existingUser = await db
+			.select()
+			.from(userTable)
+			.where(eq(userTable.email, email))
+			.limit(1)
+
+		if (existingUser.length === 0) {
+			return fail(400, { form, error: 'Incorrect email or password' })
+		}
+
+		const user = existingUser[0]
+
+		const validPassword = await verify(user.passwordHash, form.data.password, {
+			memoryCost: 19456,
+			timeCost: 2,
+			outputLen: 32,
+			parallelism: 1
+		})
+		if (!validPassword) {
+			return fail(400, {
+				form,
+				error: 'Incorrect email or password'
+			})
+		}
+
+		const session = await lucia.createSession(user.id, {})
+		const sessionCookie = lucia.createSessionCookie(session.id)
+		cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		})
+
+		throw redirect(302, '/')
+	}
+}
---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms'
+	import type { ActionData } from './$types'
+
+	export let data
+	export let form: ActionData & { error: string }
+
+	const { form: formData, enhance, errors } = superForm(data.form)
+</script>
+
+<form method="POST" use:enhance>
+	<label for="email">Email</label>
+	<input
+		type="text"
+		id="email"
+		name="email"
+		autocomplete="email"
+		aria-invalid={$errors.email ? 'true' : undefined}
+		bind:value={$formData.email}
+	/>
+	{#if $errors.email}
+		<p class="error-message">{$errors.email}</p>
+	{/if}
+	<label for="password">Password</label>
+	<input
+		type="password"
+		id="password"
+		name="password"
+		autocomplete="current-password"
+		aria-invalid={$errors.password ? 'true' : undefined}
+		bind:value={$formData.password}
+	/>
+	{#if $errors.password}
+		<p class="error-message">{$errors.password}</p>
+	{/if}
+	{#if form?.error}
+		<p class="error-message">{form.error}</p>
+	{/if}
+	<button type="submit">Continue</button>
+	<p>Don't have an account? <a href="/register">Register</a></p>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 400px;
+	}
+
+	button {
+		margin-top: 2rem;
+	}
+
+	.error-message {
+		color: red;
+	}
+</style>
