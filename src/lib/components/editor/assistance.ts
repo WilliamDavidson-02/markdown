29713fd26f5e61f2c4959e49f8b01ff5a81f38cb
William Davidson <wj.davidson@outlook.com>
2024-10-17 15:45:28 +0200

    refactor: moved editor doc manipulation functions from commands to assistance

--- src/lib/components/editor/assistance.ts
+++ src/lib/components/editor/assistance.ts
@@ -10,6 +10,7 @@ import { EditorView, ViewUpdate } from '@codemirror/view'
 import { CompletionContext, type Completion } from '@codemirror/autocomplete'
 import { languages } from '@codemirror/language-data'
 import type { LineChange } from './types'
+import { insertBlankLine } from '@codemirror/commands'
 
 const tableRegex = {
 	header: /^\|(.+\|)+/,
@@ -272,3 +273,171 @@ export const handleLinks = EditorView.domEventHandlers({
 	paste: formatLinks,
 	drop: formatLinks
 })
+
+const isBold = (value: string) => {
+	return value.startsWith('**') && value.endsWith('**')
+}
+
+const isItalic = (value: string) => {
+	return value.startsWith('*') && value.endsWith('*')
+}
+
+const selectWordBoundary = (range: SelectionRange, state: EditorState) => {
+	let { from, to } = range
+	const line = state.doc.lineAt(from)
+
+	while (from > line.from) {
+		if (!/\w/.test(line.text[from - line.from - 1])) break
+		from--
+	}
+
+	while (to < line.to) {
+		if (!/\w/.test(line.text[to - line.from])) break
+		to++
+	}
+
+	return EditorSelection.range(from, to)
+}
+
+export const toggleBold = (editor: EditorView) => {
+	const changes = editor.state.changeByRange((range) => {
+		let newRange = range
+		if (range.from === range.to) {
+			newRange = selectWordBoundary(range, editor.state)
+		}
+
+		const isSelectionBold = isBold(editor.state.doc.sliceString(newRange.from - 2, newRange.to + 2))
+		let changes: ChangeSpec[] = []
+
+		if (isSelectionBold) {
+			changes = [
+				{
+					from: newRange.from - 2,
+					to: newRange.from
+				},
+				{
+					from: newRange.to,
+					to: newRange.to + 2
+				}
+			]
+			newRange = EditorSelection.range(range.from - 2, range.to - 2)
+		} else {
+			changes = [
+				{
+					from: newRange.from,
+					insert: '**'
+				},
+				{
+					from: newRange.to,
+					insert: '**'
+				}
+			]
+			newRange = EditorSelection.range(range.from + 2, range.to + 2)
+		}
+		return { range: newRange, changes }
+	})
+
+	editor.dispatch(changes, {
+		scrollIntoView: true,
+		annotations: Transaction.userEvent.of('input')
+	})
+
+	return true
+}
+
+export const toggleItalic = (editor: EditorView) => {
+	const changes = editor.state.changeByRange((range) => {
+		let newRange = range
+		if (range.from === range.to) {
+			newRange = selectWordBoundary(range, editor.state)
+		}
+
+		const isSelectionItalic = isItalic(
+			editor.state.doc.sliceString(newRange.from - 1, newRange.to + 1)
+		)
+		let changes: ChangeSpec[] = []
+
+		if (isSelectionItalic) {
+			changes = [
+				{
+					from: newRange.from - 1,
+					to: newRange.from
+				},
+				{
+					from: newRange.to,
+					to: newRange.to + 1
+				}
+			]
+			newRange = EditorSelection.range(range.from - 1, range.to - 1)
+		} else {
+			changes = [
+				{
+					from: newRange.from,
+					insert: '*'
+				},
+				{
+					from: newRange.to,
+					insert: '*'
+				}
+			]
+			newRange = EditorSelection.range(range.from + 1, range.to + 1)
+		}
+		return { range: newRange, changes }
+	})
+
+	editor.dispatch(changes, {
+		scrollIntoView: true,
+		annotations: Transaction.userEvent.of('input')
+	})
+
+	return true
+}
+
+const isCheckBox = (value: string) => {
+	const checkBoxPattern = /^- \[(x| )\] /i
+	return checkBoxPattern.test(value)
+}
+
+export const toggleCheckbox = (line: Line, range: SelectionRange) => {
+	const startIndex = line.text.indexOf('- [')
+	const isChecked = line.text.toLowerCase().includes('- [x]')
+
+	const checkbox = isChecked ? '- [ ]' : '- [x]'
+
+	return {
+		from: line.from + startIndex,
+		to: line.from + startIndex + checkbox.length,
+		insert: checkbox
+	}
+}
+
+export const handleInsertBlankLine = (editor: EditorView) => {
+	const changes = editor.state.changeByRange((range) => {
+		let changes: ChangeSpec[] = []
+		let from = range.from
+
+		while (from <= range.to) {
+			const line = editor.state.doc.lineAt(from)
+			if (isCheckBox(line.text)) {
+				changes.push(toggleCheckbox(line, range))
+				from = line.to + 1
+				continue
+			}
+
+			break
+		}
+
+		return { range, changes }
+	})
+
+	if (changes.changes.empty) {
+		return insertBlankLine(editor)
+	}
+
+	editor.dispatch(changes, {
+		scrollIntoView: true,
+		annotations: Transaction.userEvent.of('input')
+	})
+
+	return true
+}
